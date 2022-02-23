aa2e145dd17667e73632c5fbacd159f37c2e2216
Vadym Starchenko <starvadim@ukr.net>
2022-02-23 13:35:19 +0200

    updates for genres

--- src/js/services/movieList.js
+++ src/js/services/movieList.js
@@ -1,4 +1,6 @@
 import api from './ApiService';
+import notFoundImg from '../../images/not_found_ver.jpg';
+import * as storage from './localStorage';
 
 const refs = {
     list: document.querySelector(".movies")
@@ -30,17 +32,25 @@ function resetView (){
 }
 
 function makeMovieList(array) {
+  const allGenres = storage.get('genres');
+  const markup = array.map(({ id, title, poster_path, genre_ids, name, first_air_date, release_date, vote_average }) => {
 
-    const markup = array.map(({ id, title, poster_path, name, first_air_date, release_date, vote_average}) => {
-        const year = new Date(release_date).getFullYear();
-        const year2 = new Date(first_air_date).getFullYear();
+    const genres = genre_ids.filter(id => allGenres[id])
+      .map(id=>allGenres[id])
+      .join(', ');
+
+
+      const year = new Date(release_date).getFullYear();
+      const year2 = new Date(first_air_date).getFullYear();
+      const poster = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : notFoundImg;
+
             return ` <li id='${id}' class="movies__item">
       <a href="">
-        <img class="movies__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title || name}" width="280" height="398">
+        <img class="movies__img" src="https://image.tmdb.org/t/p/w500${poster}" alt="${title || name}" width="280" height="398">
         <div class="movies__wrapper">
           <h2 class="movies__name">${title || name}</h2>
           <div class="movies__wrapper--data">
-            <span class="movies__genre">genre</span>
+            <span class="movies__genre">${genres}</span>
             <span class="movies__year">${year || year2}</span>
             <span class="movies__rating">${vote_average}</span>
           </div>
@@ -51,3 +61,5 @@ function makeMovieList(array) {
     }).join("");
     refs.list.insertAdjacentHTML('beforeend', markup);
 }
+
+export { onLoading, makeMovieList, resetView};
