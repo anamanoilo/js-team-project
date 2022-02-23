86a147fd07faceb3a4c3954aa15183e8402f7523
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-23 20:15:55 +0200

    commit

--- src/js/services/search.js
+++ src/js/services/search.js
@@ -1,33 +1,45 @@
 import api from './ApiService';
+import * as storage from './localStorage';
+import { prepareData, makeMovieList } from './movieList';
 
-
-const refs ={
-    searchForm: document.querySelector(".form"),
-    searchBtn: document.querySelector('.btn-sub'),
-    input: document.querySelector('input[name="searchQuery"]')
-}
+const refs = {
+  searchForm: document.querySelector('.search__form'),
+  searchBtn: document.querySelector('.search__btn'),
+  input: document.querySelector('.search__input'),
+  list: document.querySelector('.movies'),
+  error: document.querySelector('.search__fail'),
+};
 
 let inputValue = '';
 
 refs.searchForm.addEventListener('submit', searchFilms);
 
-
 async function searchFilms(e) {
-    e.preventDefault();
-  
-    inputValue = refs.input.value.trim();
-  
-    if (!inputValue) {
-      console.log('net');
+  e.preventDefault();
+
+  inputValue = refs.input.value.trim();
+  api.searchQuery = inputValue;
+  if (!inputValue) {
+    return;
+  }
+  loadMoviesByKeyWord();
+}
+
+async function loadMoviesByKeyWord() {
+  try {
+    const movies = await api.fetchMovieByKeyword();
+
+    if (!movies.results.length) {
+      refs.error.textContent = 'Search result not successful. Enter the correct movie name';
       return;
     }
 
-    try {
-      const res = await fetchMovieByKeyword();
-      console.log('da')
-      console.log(res)
-
-    } catch (error) {
-        console.log('net')
-    }
+    const moviesDatalist = prepareData(movies.results);
+    storage.save('moviesData', moviesDatalist);
+    refs.list.innerHTML = '';
+    makeMovieList(moviesDatalist);
+  } catch (error) {
+    console.error(error);
+    api.resetPage();
   }
+}
