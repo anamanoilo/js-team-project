8efebf1adcf2c11e6b5a61414a0cb69ae0aaf1d3
andrpoch <andrpoch25@gmail.com>
2022-03-05 23:48:05 +0200

    Добавил эффекты

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './sass/main.scss';
 import './js/services/ApiService';
 import './js/services/movieList';
 import './js/components/pagination';
+import './js/components/theme-switch';
 import './js/services/search';
---
+++ src/js/components/theme-switch.js
@@ -0,0 +1,22 @@
+const refs = {
+   body: document.body,
+   span: document.querySelector('#click'),
+   checkbox: document.querySelector('#checkbox'),
+   theme: document.querySelector('.theme-switch'),
+}
+refs.theme.addEventListener('click', onSwitch)
+function onSwitch() {
+   if (refs.checkbox.checked) {
+      refs.body.classList.toggle('dark-theme')
+      localStorage.setItem('theme', 'dark-theme');
+   } else {
+      localStorage.clear();
+   }
+}
+function currentTheme() {
+  if (localStorage.getItem('theme') === 'dark-theme') {
+    refs.checkbox.checked = true;
+    refs.body.classList.add('dark-theme');
+  }
+}
+currentTheme();
