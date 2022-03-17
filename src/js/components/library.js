89d8837166b27fb2be92f80ef1d424e4d060e069
andrpoch <andrpoch25@gmail.com>
2022-03-17 12:48:16 +0200

    Исправил тему на модалке и забрал локал.

--- src/js/components/library.js
+++ src/js/components/library.js
@@ -9,7 +9,6 @@ const refs = {
    queueBtn: document.querySelector('.queue__link'),
 }
 function onHome() {
-   localStorage.removeItem('lib','library')
    refs.header.classList.remove('library');
    refs.search.classList.remove('visually-hidden');
    refs.libraryBtn.classList.add('visually-hidden');
@@ -18,7 +17,6 @@ function onHome() {
    
 }
 function onLibrary() {
-   localStorage.setItem('lib', 'library');
    refs.header.classList.add('library');
    refs.search.classList.add('visually-hidden');
    refs.libraryBtn.classList.remove('visually-hidden');
@@ -28,25 +26,12 @@ function onLibrary() {
 function watchedBtn() {
    refs.watchedBtn.classList.add('is-active');
    refs.queueBtn.classList.remove('is-active');
-   localStorage.removeItem('queue');
 }
 function queueBtn() {
-   localStorage.setItem('queue', 'is-active')
    refs.watchedBtn.classList.remove('is-active');
    refs.queueBtn.classList.add('is-active');
 }
-function currentBtn() {
-   if (localStorage.getItem('queue') === 'is-active') {
-    queueBtn();
-   }
-}
-function currentPage() {
- if (localStorage.getItem('lib') === 'library') {
-    onLibrary();
- }
-}
-currentBtn();
-currentPage();
+
 refs.watchedBtn.addEventListener('click', watchedBtn);
 refs.queueBtn.addEventListener('click', queueBtn);
 refs.logo.addEventListener('click', onHome);
